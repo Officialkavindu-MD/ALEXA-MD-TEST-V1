37f327cae649caef7445e670b25cb7f91e2685ca
© ᴏꜰꜰɪᴄɪᴀʟ ᴋᴀᴠɪɴᴅᴜ-ᴍᴅ <113903400+Officialkavindu-MD@users.noreply.github.com>
2024-09-02 21:34:37 +0530

    Update chatCounter.js

--- plugins/chatCounter.js
+++ plugins/chatCounter.js
@@ -1,1 +1,38 @@
-Aa
+const fs = require('fs');
+const path = require('path');
+
+// Create or load the chat data from the RunningData folder
+const dataDir = path.join(__dirname, '../RunningData');
+if (!fs.existsSync(dataDir)) {
+    fs.mkdirSync(dataDir);
+}
+
+const chatCountFile = path.join(dataDir, 'chatCount.json');
+let chatCount = {};
+
+if (fs.existsSync(chatCountFile)) {
+    chatCount = JSON.parse(fs.readFileSync(chatCountFile, 'utf-8'));
+}
+
+// Function to update chat count for each number
+function updateChatCount(message) {
+    const chatId = message.from;
+    const author = message.author || message.from; // For private chats, author will be undefined
+
+    if (!chatCount[chatId]) {
+        chatCount[chatId] = {};
+    }
+
+    if (!chatCount[chatId][author]) {
+        chatCount[chatId][author] = 0;
+    }
+
+    chatCount[chatId][author] += 1;
+
+    // Save updated chat count to file
+    fs.writeFileSync(chatCountFile, JSON.stringify(chatCount, null, 2));
+}
+
+module.exports = {
+    updateChatCount,
+};
